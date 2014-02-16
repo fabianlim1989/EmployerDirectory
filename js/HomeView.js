49b267c656929f89b63bf2a30aceef5b0c0abb9e
Fabian Lim <fabianlim1989@gmail.com>
2014-02-15 19:23:31 -0500

    try at changing content to list park but failed

--- js/HomeView.js
+++ js/HomeView.js
@@ -3,6 +3,7 @@ var HomeView = function(store) {
   	this.initialize = function() {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
         this.el = $('<div/>');
+        //this.el.on('keyup', '.search-key', this.findByName);
         this.el.on('keyup', '.search-key', this.findByName);
     };
  	
@@ -14,15 +15,20 @@ var HomeView = function(store) {
 	this.findByName = function() {
 	    store.findByName($('.search-key').val(), function(employees) {
 	        $('.employee-list').html(HomeView.liTemplate(employees));
-	        if (self.iscroll) {
+	        /*if (self.iscroll) {
 	            console.log('Refresh iScroll');
 	            self.iscroll.refresh();
 	        } else {
 	            console.log('New iScroll');
 	            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
-	        }
+	        }*/
     	});
 	};
+
+	this.listParks = function() {
+        $('.parks-list').html(HomeView.liTemplate(employees));
+	};
+
 
     this.initialize();
 
